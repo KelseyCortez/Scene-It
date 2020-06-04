e6b8dbee0e4d61b4f9352c203a270b3e3d924f7f
KelseyCortez <kelscortez@gmail.com>
2020-06-04 16:22:26 -0400

    refactor

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
   function renderMovies(movieArray) {
     const moviesHtmlArray = movieArray.map(movie => {
       return `<div class="movie">
-            <div class="card" style="width: 18rem;">
-              <img src="${movie.Poster}" class="movie-poster" alt= img src="no_image.png">
-              <div class="card-body">
-                <h5 class="movie-title">${movie.Title}</h5>
-                <p class="release-date">${movie.Year}</p>
-                <button onclick="saveToWatchList('${movie.imdbID}')">Add to Watchlist</button>
+            <div class="card" style="width: 18rem; height: 65%"">
+              <img class="card-img-top w-100 h-100" src="${movie.Poster}" class="movie-poster" onerror="if (this.src != 'no_image.png') this.src='/no_image.png';" alt="Card image cap">
+              <div class="card-body d-flex flex-column justify-content-between align-items-center p-0 pt-2 m-0 overflow-auto" style="height: 27%"">
+                <h5 class="movie-title text-center m-0 p-0 font-weight-bold">${movie.Title}</h5>
+                <p class="release-date dropdown d-flex align-items-center m-0 p-0 pt-2">${movie.Year}</p>
+                <button onclick="saveToWatchList('${movie.imdbID}')">Add to Watch list</button>
               </div>
             </div>
           </div>`
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
         movieDataArray = response.data.Search;
         console.log(movieDataArray);
         moviesContainer.innerHTML = renderMovies(movieDataArray);
-      
+
       })
 
   })
--- watchlist.js
+++ watchlist.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const moviesHtmlArray = movieArray.map(movie => {
             return `<div class="movie">
             <div class="card" style="width: 18rem;">
-              <img src="${movie.Poster}" class="movie-poster" alt="...">
+              <img src="${movie.Poster}" class="movie-poster" onerror="if (this.src != 'no_image.png') this.src='assets/no_image.png';" alt="Card image cap">
               <div class="card-body">
                 <h5 class="movie-title">${movie.Title}</h5>
                 <p class="release-date">${movie.Year}</p>
